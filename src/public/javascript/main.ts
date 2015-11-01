926e1bec85d80b09cb1fab7a5b18fd185a280dc7
Григорий Чипура <tchipoura@gmail.com>
2015-11-01 19:05:10 +0300

    New commit

---
+++ src/public/javascript/main.ts
@@ -0,0 +1,8 @@
+window.onload = function(){
+	console.log('Поздравляю!\n' + 
+				'Вы вы настроили проект для\n\n' + 
+				'работы с Typescript.');
+	alert('Поздравляю!\n' + 
+		  'Вы вы настроили проект для\n\n' + 
+		  'работы с Typescript.');
+}
